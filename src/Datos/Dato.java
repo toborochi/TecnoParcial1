c38067a9eecf23b130c902eb6a4784e96edd16d8
cartory <xXcartoryXx@gmail.com>
2021-03-04 01:47:10 -0400

    regexp

--- src/Datos/Dato.java
+++ src/Datos/Dato.java
@@ -1,39 +1,79 @@
 package Datos;
 
 import Datos.DConexion;
+import java.util.regex.*;
 import java.sql.ResultSet;
-import java.util.LinkedList;
 
 public class Dato {
- 
-    DConexion dbc;
-    String table_name;
-    String[] columns;
     
-    public Dato(String TableName, String[] Columns){
-        this.columns=Columns;
-        this.table_name=TableName;
-        this.dbc = new DConexion();
+    
+    DConexion dbc;
+    String TABLE;
+    String[] COLUMNS;
+
+    private boolean isNumber(Object arg) {
+        String regex = "^(\\d+(\\.\\d+)?)$";
+        return Pattern.matches(regex, String.valueOf(arg));
     }
     
-    public ResultSet listar(){
-        String sql = String.format("SELECT * FROM %s ", table_name);
+    public Dato(String table, String[] Columns) {
+        this.TABLE = table;
+        this.COLUMNS = Columns;
+        this.dbc = new DConexion();
+    }
+
+    public ResultSet listar() {
+        String sql = "SELECT * FROM " + TABLE;
+        return dbc.query(sql);
+    }
+
+    public ResultSet crear(Object args[]) {
+        String COLS = "";
+        String VALUES = "";
+
+        String regex = "^(\\d+(\\.\\d+)?)$";
+        
+        for (int i = 0; i < COLUMNS.length; i++) {
+            if (!isNumber(args[i])) {
+                args[i] = "'" + args[i] + "'"; 
+            }
+            
+            if (i == COLUMNS.length -1) {
+                VALUES += "%s";
+                COLS += COLUMNS[i];
+            } else {
+                VALUES += "%s,";
+                COLS += COLUMNS[i] + ",";
+            }                      
+        }
+
+        String sql = String.format(
+            "INSERT INTO " + TABLE + "(" + COLS + ") VALUES (" + VALUES + ")",
+            args
+        );
         
         return dbc.query(sql);
     }
-    
-    public ResultSet crear(){
-        String sql = String.format("SELECT * FROM %s ", table_name);
+
+    public ResultSet editar(Object args[]) {
+        String VALUES = "";
+        for (int i = 0; i < COLUMNS.length -1; i++) {
+            if (!isNumber(args[i])) {
+                args[i] = "'" + args[i] + "'";
+            }
+            VALUES += (i == COLUMNS.length - 2) ? "= %s, " :" = %s ";
+        }
+        
+        String sql = String.format(
+            "UPDATE " + TABLE + "SET " + VALUES + "WHERE id = %s",
+            args
+        );
+        
         return dbc.query(sql);
     }
-    
-    public ResultSet editar(){
-        String sql = String.format("SELECT * FROM %s ", table_name);
-        return dbc.query(sql);
-    }
-    
-    public ResultSet eliminar(String id){
-        String sql = String.format("DELETE FROM %s WHERE %s = %s;", table_name,columns[0],id);
+
+    public ResultSet eliminar(String id) {
+        String sql = String.format("DELETE FROM "+ TABLE +" WHERE id = %s", id);
         return dbc.query(sql);
     }
 }
