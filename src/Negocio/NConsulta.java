6798e060da26dc8c595cd2d9c595f1eb525fdc83
cartory <xXcartoryXx@gmail.com>
2021-03-05 02:03:17 -0400

    Merge origin/main into main

---
+++ src/Negocio/NConsulta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import Datos.DConsulta;
+
+/**
+ *
+ * @author cartory
+ */
+public class NConsulta extends Negocio {  
+    public NConsulta() {
+        super(new DConsulta());
+    }
+}
---
+++ src/Negocio/NTratamiento.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Negocio;
+
+import Datos.DTratamiento;
+
+/**
+ *
+ * @author cartory
+ */
+public class NTratamiento extends Negocio{
+
+    public NTratamiento() {
+        super(new DTratamiento());
+    }
+    
+}
