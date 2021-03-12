17252253d444152696f6c32d4f7b1babb47a41bf
cartory <xXcartoryXx@gmail.com>
2021-03-12 11:12:03 -0400

    Merge origin/main into main
    Conflicts:
    	src/tecnoweb_basic_email/EmailTask.java

--- src/tecnoweb_basic_email/EmailTask.java
+++ src/tecnoweb_basic_email/EmailTask.java
@@ -161,7 +161,8 @@ public class EmailTask implements Callable<MailSender> {
                 mensaje=this.help();
                 break;
             case "reporteGeneros":
-                mensaje = "<img \n"
+                mensaje = "\"Content-Type: text/html; charset=\\\"UTF-8\\\"\\n\""
+                        + "<img \n"
                         + "     style=\"-webkit-user-select: none; display: block; margin: auto; padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left); cursor: zoom-in;\"\n"
                         + "     src=\"https://quickchart.io/chart?bkg=white&amp;c={\n"
                         + "          type:%27pie%27,\n"
